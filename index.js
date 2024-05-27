d680bc4cea1b8c028f914d5ca6d6a1e77e3aaca9
Maycon_Ryan <mayconryan888@gmail.com>
2024-05-27 11:19:15 -0400

    alteracao_final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let idade = prompt('digite sua idade:');
 let convert = parseInt(idade);
-if (convert <= 18) {
-    console.log('Acesso Negado!.');
+if (convert >= 18) {
+    console.log('Acesso Liberado, Seja Bem Vindo!');
 } else {
-    console.log('Acesso Liberado, Seja Bem Vindo!!');
+    console.log('Acesso negado. Você deve ter 18 anos ou mais para acessar!.');
 }
